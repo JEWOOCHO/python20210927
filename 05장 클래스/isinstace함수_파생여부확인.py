e7eb0b2e66a8c6ec3a58765eab9e588a40af90ee
JEWOOCHO <jewoocho@gmail.com>
2021-09-28 15:43:59 +0900

    git 연습

--- 05장 클래스/isinstace함수_파생여부확인.py
+++ 05장 클래스/isinstace함수_파생여부확인.py
@@ -8,6 +8,7 @@ p, s = Person(), Student()
 
 print("p is instance of Person: ", isinstance(p, Person))
 print("s is instance of Person: ", isinstance(s, Person))
+print("s is instance of Person: ", isinstance(s, Student))
 print("p is instance of Object: ", isinstance(p, object))
 print("p is instance of Bird: ", isinstance(p, Bird))
 print("int is instance of Object: ", isinstance(int, object))
